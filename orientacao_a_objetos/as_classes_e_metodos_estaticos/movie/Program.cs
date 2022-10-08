d319a09932b8635ef223ccd0837f1ff3587d3b17
Patrick <patrickbastosc@gmail.com>
2022-10-08 18:29:32 -0300

    Content: Abstraction static classes and methods

---
+++ orientacao_a_objetos/as_classes_e_metodos_estaticos/movie/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace cine;
+
+class Program 
+{
+  public static void Main(string[] args){
+    var movie = new Movie("Matrix", "Ficção Científica");
+    var movie2 = new Movie("Senhor dos Aneis", "Fantasia");
+
+    Console.WriteLine(movie.Title);
+    Console.WriteLine(movie.Category);
+
+    Console.WriteLine(movie2.Title);
+    Console.WriteLine(movie2.Category);
+
+    movie.Title = "Matrix 2";
+    movie.Category = "Pudim"; // Exception lançada no terminal!!!
+
+    Console.WriteLine(movie.Title);
+    Console.WriteLine(movie.Category);
+  }
+}
